0de2ca39bf77415c0b8a37c2f10cc03b0aaebf6e
aar0njv <aaronjoy382@gmail.com>
2025-09-07 00:09:55 +0530

    router updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import Dashboard from './pages/Dashboard'
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <AuthPage /> } />
+          <Route path="/dashboard" element={ <Dashboard /> } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
