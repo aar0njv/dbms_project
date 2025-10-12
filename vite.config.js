6c4746bb6f0ae116a5d0aeac4cbdfe1d7d88b38d
aar0njv <aaronjoy382@gmail.com>
2025-10-12 14:06:38 +0530

    conf

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Enable history API fallback for SPA routing
-    historyApiFallback: true,
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 })
